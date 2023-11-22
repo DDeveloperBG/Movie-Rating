3965cb4d1585bb3506a90955a72c5896fe01834a
Daniel Yordanov <dakataebg@students.softuni.bg>
2023-11-22 18:24:54 +0200

    Added RatingControl stars count change on click

--- MovieRatingApp/Controls/RatingControl.xaml.cs
+++ MovieRatingApp/Controls/RatingControl.xaml.cs
@@ -6,7 +6,7 @@ namespace MovieRatingApp.Controls
     {
         const string STAR_IMG_SRC = "Resources/Images/star_icon.svg";
         const string EMPTY_IMG_COLOR = "white";
-        const int MAX_STARS_COUNT = 5;
+        const byte MAX_STARS_COUNT = 5;
 
         public static readonly BindableProperty StarsCountProperty =
             BindableProperty.Create(nameof(StarsCount), typeof(int), typeof(RatingControl), defaultValue: 0, propertyChanged: OnStarsCountPropertyChanged);
@@ -40,6 +40,17 @@ namespace MovieRatingApp.Controls
             set => SetValue(ImagesProperty, value);
         }
 
+        public void OnButtonClicked(object sender, EventArgs args)
+        {
+            var imageButton = (ImageButton)sender;
+
+            var starNumber = (byte)imageButton.CommandParameter;
+            if (StarsCount != starNumber)
+            {
+                StarsCount = starNumber;
+            }
+        }
+
         private static void OnStarsCountPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             var control = (RatingControl)bindable;
@@ -49,12 +60,13 @@ namespace MovieRatingApp.Controls
         private void UpdateImages()
         {
             List<RatingViewImage> images = new();
-            for (int i = 0; i < MAX_STARS_COUNT; i++)
+            for (byte i = 1; i <= MAX_STARS_COUNT; i++)
             {
                 RatingViewImage image = new()
                 {
+                    Number = i,
                     ImageUrl = STAR_IMG_SRC,
-                    Color = i < StarsCount ? Color : EMPTY_IMG_COLOR,
+                    Color = i > StarsCount ? EMPTY_IMG_COLOR : Color,
                 };
 
                 images.Add(image);
--- MovieRatingApp/Models/ViewDTOs/RatingViewImage.cs
+++ MovieRatingApp/Models/ViewDTOs/RatingViewImage.cs
@@ -3,6 +3,8 @@ namespace MovieRatingApp.Models.ViewDTOs
 {
     public class RatingViewImage
     {
+        public byte Number { get; set; }
+
         public string ImageUrl { get; set; }
 
         public string Color { get; set; }
